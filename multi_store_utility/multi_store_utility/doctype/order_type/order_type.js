55466a429c6611b40766628de5d13ec44f6a6ecd
Geethu K <geethuk626@gmail.com>
2024-08-19 12:07:28 +0530

    feat:Created Doctype Order Type & Adding Custom field in Item Dectype

---
+++ multi_store_utility/multi_store_utility/doctype/order_type/order_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, efeone Pvt Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Order Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ multi_store_utility/multi_store_utility/doctype/order_type/order_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, efeone Pvt Ltd and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class OrderType(Document):
+	pass
